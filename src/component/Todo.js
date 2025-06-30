ec238ecd574a242e36d3347969f99fb8e08fe4c9
Vpal72061@gmail.com <Vpal72061@gmail.com>
2025-06-30 15:18:57 +0530

    icon status

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,27 +1,49 @@
 // Todo.js
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPenToSquare,
+  faTrash,
+  faClock,         // 📝  todo
+  faSpinner,       // ⏳  in progress
+  faCheck          // ✅  done
+} from "@fortawesome/free-solid-svg-icons";
 
 export const Todo = ({ task, deleteTodo, editTodo, toggleComplete }) => {
+  const { id, status = "todo" } = task;  // default 'todo' if missing
+
+  // choose icon based on status
+  const statusIcon =
+    status === "todo"       ? faClock   :
+    status === "inprogress" ? faSpinner :
+    faCheck;  // done
+
   return (
-    <div className="Todo">
+    <div className={`Todo status-${status}`}>
+      {/* badge */}
+      <span className="status-badge">
+        <FontAwesomeIcon icon={statusIcon} />
+      </span>
+
+      {/* main task text */}
       <p
         className={task.completed ? "completed" : "incompleted"}
-        onClick={() => toggleComplete(task.id)}
+        onClick={() => toggleComplete(id)}
       >
         {task.task}
       </p>
+
+      {/* edit / delete */}
       <div>
         <FontAwesomeIcon
           className="edit-icon"
           icon={faPenToSquare}
-          onClick={() => editTodo(task.id)}
+          onClick={() => editTodo(id)}
         />
         <FontAwesomeIcon
           className="delete-icon"
           icon={faTrash}
-          onClick={() => deleteTodo(task.id)}
+          onClick={() => deleteTodo(id)}
         />
       </div>
     </div>
