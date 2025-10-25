7f9b9a047f7b8d453064cd2d5eea583992fa3dba
Deepanjali Tripathi <monucode@users.noreply.github.com>
2025-10-25 10:17:27 +0530

    setup app

---
+++ android/app/src/main/java/com/hexamadtodo/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.hexamadtodo.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hexamadtodo.app',
+  appName: 'HexaToDo',
+  webDir: 'build'
+};
+
+export default config;
